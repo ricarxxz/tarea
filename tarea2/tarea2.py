8dc9d21e3e28f16aff11a52fc0c944425ba4269a
Ricardo Cogollo <ricardocogollo989@gmail.com>
2026-02-19 12:41:01 -0500

    no

--- tarea2/tarea2.py
+++
@@ -1,7 +0,0 @@
-import os 
-os.system("cls")
-rutas= [("centro","marbella"):{"distancia":0, "valor":10000},
-       ("centro","torices"):{"distancia":15, "valor":25000},
-       ("centro","bicentenario"):{"distancia":30, "valor":70000}]
-def motor_de_rutas():
-    promedio = 1
